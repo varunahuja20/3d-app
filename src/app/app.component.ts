d8def9979e8da3cb833c188c8fd4cba48e8eba5c
VARUN AHUJA <varunahuja.20@gmail.com>
2023-04-26 23:49:00 +0530

    added gridHelper and orbitControls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 @Component({
   selector: 'app-root',
@@ -16,14 +17,28 @@ export class AppComponent {
   cube = new THREE.Mesh(this.geometry, this.material)
   ngOnInit() {
 
+    //adding grid helper
+    const size = 10;
+    const divisions = 10;
+    const gridHelper = new THREE.GridHelper(size, divisions);
+    this.scene.add(gridHelper);
+
+    //adding orbital controls
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+		controls.listenToKeyEvents( window ); // optional
+    //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+	  // controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+		// controls.dampingFactor = 0.05;
+    controls.screenSpacePanning = false;
+    // controls.minDistance = 100;
+		// controls.maxDistance = 500;
+    // controls.maxPolarAngle = Math.PI / 2;
+
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);  
+    document.body.appendChild(this.renderer.domElement);
     this.scene.add(this.cube);
-
     this.camera.position.z = 5;
-
     this.animate();
-    // animate();
   }
   animate = () => {
     requestAnimationFrame(this.animate);
